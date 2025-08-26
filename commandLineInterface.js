8e4050a36f9ce41f7a4fb8ebb323ad85c462f073
emerson-feral <emerson_feral@hotmail.com>
2025-08-25 21:44:30 -0300

    Adding "init" to CLI

--- commandLineInterface.js
+++ commandLineInterface.js
@@ -1,14 +1,18 @@
 const fileStatus = require("./fileStatus");
-const fileDetection = require("./fileDetection");
+const { fileInitiation } = require("./fileInitiation");
 const args = process.argv.slice(2);
 
 switch (args[0]) {
+  case "init":
+    //SETUP
+    fileInitiation();
+    fileDetection.main();
   case "add":
     //ADD FILE
+    fileStatus.newFileStatus(args[1]);
     break;
   case "status":
-  // fileDetection();
-  // fileStatus.unstagedStatus();
+    fileStatus.status();
   // process.exit(0);
 
   default:
