4732a047b73a04cdc8f1da1e1be85ef2d1f824d3
emerson-feral <emerson_feral@hotmail.com>
2025-08-26 21:03:51 -0300

    Adding the initial logic

--- fileInitiation.js
+++ fileInitiation.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 const path = require("path");
+const fileDetection = require("./fileDetection");
 const folderStateFile = "lastState.json";
 const stagedFiles = "stagedFiles.json";
 const dir = "./";
 
 function fileInitiation() {
+  const { newFiles } = fileDetection.main();
   fs.writeFileSync(
     path.join(dir, folderStateFile),
-    JSON.stringify([], null, 2)
+    JSON.stringify(newFiles, null, 2)
   );
   fs.writeFileSync(path.join(dir, stagedFiles), JSON.stringify([], null, 2));
 }
