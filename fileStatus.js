2d8cd03126bcde42434bf7e8bda50bfbcbe25dd5
emerson-feral <emerson_feral@hotmail.com>
2025-08-26 21:06:21 -0300

    Adding the fileStatus logic to untracked and staged files

--- fileStatus.js
+++ fileStatus.js
@@ -1,12 +1,67 @@
 const fs = require("fs");
 const path = require("path");
+const lastState = require("./lastState.json");
+const stagedFiles = require("./stagedFiles.json");
 
-function newFileStatus(file) {
-  // Add files to stagedFiles
+const rootPath = "./";
+const stagedFilesPath = "stagedFiles.json";
+const lastStatePath = "lastState.json";
+
+function addFileToStage(file) {
+  const data = fs.readFileSync(path.join(rootPath, stagedFilesPath), "utf-8");
+  const arrayData = JSON.parse(data);
+
+  arrayData.push(file);
+  fs.writeFileSync(
+    path.join(rootPath, stagedFilesPath),
+    JSON.stringify(arrayData, null, 2)
+  );
+
+  removeUntrackedFile(file);
+}
+
+function addFileToUntracked(file) {
+  const data = fs.readFileSync(path.join(rootPath, lastStatePath), "utf-8");
+  const arrayData = JSON.parse(data);
+  arrayData.push(file);
+
+  fs.writeFileSync(
+    path.join(rootPath, lastStatePath),
+    JSON.stringify(arrayData, null, 2)
+  );
 }
 
 function status() {
-  // Show status
+  console.log("Untracked files: ", lastState);
+  console.log("Staged files: ", stagedFiles);
 }
 
-module.exports = { newFileStatus, status };
+function removeUntrackedFile(file) {
+  const data = fs.readFileSync(path.join(rootPath, lastStatePath), "utf-8");
+  const lastStateFiles = JSON.parse(data);
+
+  const removedUntrackedFile = lastStateFiles.filter(
+    (fileName) => fileName != file
+  );
+
+  fs.writeFileSync(
+    path.join(rootPath, lastStatePath),
+    JSON.stringify(removedUntrackedFile, null, 2)
+  );
+}
+
+function removeStagedFile(file) {
+  const data = fs.readFileSync(path.join(rootPath, stagedFilesPath), "utf-8");
+  const stagedFiles = JSON.parse(data);
+
+  const removeStagedFile = stagedFiles.filter((fileName) => fileName != file);
+
+  fs.writeFileSync(
+    path.join(rootPath, stagedFilesPath),
+    JSON.stringify(removeStagedFile, null, 2)
+  );
+
+  addFileToUntracked(file);
+}
+
+module.exports = { addFileToStage, removeStagedFile, status };
